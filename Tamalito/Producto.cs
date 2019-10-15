8b4650c2f98a7204a2a5f31d2ad1fa66c8ab1fde
ANGEL ABRAHAM ZATARAIN LOPEZ <azatarai@itam.mx>
2019-10-15 16:00:42 -0500

    Clase Pedido, clase Producto añadidas

---
+++ Tamalito/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamalito
+{
+    class Producto
+    {
+        private int IdProducto { get; set; }
+        private int costo { get; set; }
+        private int inventario { get; set; }
+        private String nombre { get; set; }
+        private String descripcion { get; set; }
+        private String categoria { get; set; }
+        private String urlImagen { get; set; }
+        private int tiempoPreparacion { get; set; }
+
+    }
+}
